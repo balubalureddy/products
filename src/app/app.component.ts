d11f44187699d38e22ea7454e3d32c1fa778c788
charuchandu <charuchandu88908@gmail.com>
2024-04-29 18:37:53 +0530

    changed the title property and added one element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'products';
+  title = 'products project';
   username:string="Jhon";
   handleChange(event:any){
     console.log(event)
