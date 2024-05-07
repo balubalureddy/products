dbc4357e26548c00bbcb3c49e02349b8595d7e5c
charuchandu <charuchandu88908@gmail.com>
2024-05-07 18:32:31 +0530

    added validations for add product form

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -17,9 +17,9 @@ export class AddProductComponent implements OnInit{
     this.addProductForm = new FormGroup({
       title: new FormControl('',[Validators.required,Validators.maxLength(10),Validators.minLength(2)]),
       brand: new FormControl('',[Validators.required]),
-      category: new FormControl('',[Validators.required]),
-      price: new FormControl('',[Validators.required,Validators.max(100000),Validators.min(1)]),
-      discountPercentage: new FormControl(),
+      category: new FormControl('SmartPhone',[Validators.required]),
+      price: new FormControl('',[Validators.required,Validators.max(100000),Validators.min(1),Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
+      discountPercentage: new FormControl('',[Validators.required,Validators.max(100000),Validators.min(1)]),
       stock: new FormControl(),
       description: new FormControl(),
     })
@@ -28,18 +28,23 @@ export class AddProductComponent implements OnInit{
   get title(){
     return this.addProductForm.get('title')!;
   }
-
+  submitClicked:Boolean=false;
   submit(){
+    this.submitClicked = true;
+    console.log(this.addProductForm,this.addProductForm.value)
     if(this.addProductForm.invalid){
       for(const control of Object.keys(this.addProductForm.controls)){
         this.addProductForm.controls[control].markAsTouched();
       }
       return;
     }
-    console.log(this.addProductForm,this.addProductForm.value)
+    
   }
 
   reset(){
+    // this.addProductForm.setValue({title:"S24 Ultra",brand:"Samsung",category:"SmartPhone",price:"10000",discountPercentage:"5",stock:"1000",description:"something"});
+    // this.addProductForm.patchValue({title:"S24 Ultra",brand:"Samsung"});
+    // this.addProductForm.get('title')?.setValue("samsung")
     this.addProductForm.reset();
     console.log(this.addProductForm.value);
   }
